ab0529af59b875408244ad2fea27a096e8451354
abdelhadia72 <thisisavnb@gmail.com>
2024-05-02 22:57:42 +0100

    fixing code no style

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,71 +1,56 @@
 #!/usr/bin/python3
+"""
+Log Parsing
+"""
 
-"""
-file stats
-"""
 import sys
 import re
-import signal
-
-pattern = r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})\] \"GET \/projects\/\d+ HTTP\/1\.1\" (\d{3}) (\d+)$"
-status_objects = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
 
 
-def signal_handler(sig, frame):
-    print_statistics()
-    sys.exit(0)
+class LogParser:
+    def __init__(self):
+        self.regex = re.compile(
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)')
+        self.line_count = 0
+        self.log = {
+            "file_size": 0,
+            "code_frequency": {
+                200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0
+            }
+        }
+
+    def output_statistics(self):
+        print("File size:", self.log["file_size"])
+        for code, count in sorted(self.log["code_frequency"].items()):
+            if count:
+                print(f"{code}: {count}")
+
+    def parse_line(self, line):
+        match = self.regex.fullmatch(line)
+        if match:
+            code = int(match.group(1))
+            file_size = int(match.group(2))
+
+            # File size
+            self.log["file_size"] += file_size
+
+            # Status code
+            if code in self.log["code_frequency"]:
+                self.log["code_frequency"][code] += 1
+
+            self.line_count += 1
+
+    def parse_logs(self):
+        try:
+            for line in sys.stdin:
+                line = line.strip()
+                self.parse_line(line)
+                if self.line_count % 10 == 0:
+                    self.output_statistics()
+        finally:
+            self.output_statistics()
 
 
-def print_statistics():
-    print(f"File size: {file_size}")
-    for key in sorted(status_objects.keys()):
-        if status_objects[key] != 0:
-            print("{}: {}".format(key, status_objects[key]))
-
-
-tracker = 0
-file_size = 0
-interrupt = False
-
-signal.signal(signal.SIGINT, signal_handler)
-
-for line in sys.stdin:
-    sline = line.strip()
-
-    if interrupt:
-        print_statistics()
-        interrupt = False
-        file_size = 0
-        status_objects = {
-            200: 0,
-            301: 0,
-            400: 0,
-            401: 0,
-            403: 0,
-            404: 0,
-            405: 0,
-            500: 0}
-        continue
-
-    if tracker == 10:
-        print_statistics()
-        tracker = 0
-
-    match = re.match(pattern, sline)
-    if match:
-        status_code = int(match.group(3))
-        if status_code in status_objects:
-            status_objects[status_code] += 1
-        file_size += int(match.group(4))
-        tracker += 1
-    else:
-        print("Line does not match expected pattern:", sline)
+if __name__ == "__main__":
+    parser = LogParser()
+    parser.parse_logs()
